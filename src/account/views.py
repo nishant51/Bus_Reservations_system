cef65d92b0c9345c6faa315feaaadd3704398fd0
nishant <timsinanishant51@gmail.com>
2024-03-09 11:56:04 +0545

    payment added

--- src/account/views.py
+++ src/account/views.py
@@ -1,6 +1,8 @@
+from telnetlib import LOGOUT
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse
+from django.shortcuts import redirect
 from django.http import HttpResponseRedirect
 from bookTicket.models import bookAticket
 from bus.models import BusInfo, BusPickArea, BusDropArea
@@ -26,14 +28,41 @@ def register(request, template_name="registration/register.html"):
     
     page_title = 'Registration'
     return render(request, template_name, locals())
+import uuid
 
 @login_required
 def my_account(request, template_name="registration/my_account.html"):
     page_title = 'My Account'
+    uid = uuid.uuid4()
+
+    url ="https://uat.esewa.com.np/epay/transrec"
+    q = request.GET.get('ticket_id')
+    d = {
+        'amt': request.GET.get('amt'),
+        'scd': 'EPAYTEST',
+        'rid': request.GET.get('refId'),
+        'pid': request.GET.get('oid'),
+    }
+    resp = requests.post(url, d)
+    print("status = ",resp.status_code )
+    if resp.status_code == 200:
+        # Get the transaction ID (pid) from the request
+        transaction_id = request.GET.get('oid')
+        # Update the status of the specific ticket
+        try:
+            ticket = bookAticket.objects.get(id=q, user=request.user)
+            ticket.status = bookAticket.CONFIRMED
+            ticket.save()
+        except bookAticket.DoesNotExist:
+            # Handle the case where the ticket with the provided transaction ID does not exist
+            pass
     tickets = bookAticket.objects.filter(user=request.user)
-    name = request.user.username
+    for ticket in tickets:
+        if ticket.status == bookAticket.CONFIRMED:
+            ticket.payment_complete = True
     return render(request, template_name, locals())
 
+import requests
 @login_required
 def ticket_details(request, ticket_id, template_name="registration/ticketdetails.html"):
     page_title = 'Ticket details'
@@ -48,7 +77,7 @@ from django.views.decorators.http import require_POST, require_GET
 
 @require_POST
 def logout_view(request):
-    logout(request)
+    LOGOUT(request)
     return redirect('login')
 
 
